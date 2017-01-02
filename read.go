d13195675238e00453ba695daf1484ffa09d6b7b
Nat Wilson <njwilson23@gmail.com>
2017-01-02 14:59:51 -0800

    redesign output from UnmarshalGeoJSON
    Previously, would return a GeoJSONResult, which had a Geometries field
    and Features/FeatureCollection fields 9containing slices). Searching the
    Geometries slice would require making type assertions on each element
    and checking whether they succeeded.
    
    Now, returns an GeoJSONContents struct, with a seperate field for each
    non-collection type.

--- read.go
+++ read.go
@@ -10,9 +10,14 @@ import (
 )
 
 // Option type for returning the components of a GeoJSON object
-type GeoJSONResult struct {
-	Features   []Feature
-	Geometries []Geometry
+type GeoJSONContents struct {
+	Points           []Point
+	LineStrings      []LineString
+	Polygons         []Polygon
+	MultiPoints      []MultiPoint
+	MultiLineStrings []MultiLineString
+	MultiPolygons    []MultiPolygon
+	Features         []Feature
 }
 
 // unknownGeoJSONType is used to read in only the type member of a GeoJSON
@@ -29,14 +34,85 @@ type partialGeometryCollection struct {
 }
 
 // UnmarshalGeoJSON unpacks Features and Geometries from a JSON byte array
-func UnmarshalGeoJSON(data []byte) (GeoJSONResult, error) {
+func UnmarshalGeoJSON(data []byte) (GeoJSONContents, error) {
 	var uknType unknownGeoJSONType
-	var result GeoJSONResult
+	var result GeoJSONContents
 	err := json.Unmarshal(data, &uknType)
 	if err != nil {
 		return result, err
 	}
 	switch uknType.Type {
+	case "Point":
+		var pt Point
+		err = json.Unmarshal(data, &pt)
+		if err != nil {
+			return result, errors.New("invalid GeoJSON: malformed Point")
+		}
+		result.Points = append(result.Points, pt)
+
+	case "LineString":
+		var ls LineString
+		err = json.Unmarshal(data, &ls)
+		if err != nil {
+			return result, errors.New("invalid GeoJSON: malformed LineString")
+		}
+		result.LineStrings = append(result.LineStrings, ls)
+
+	case "Polygon":
+		var poly Polygon
+		err = json.Unmarshal(data, &poly)
+		if err != nil {
+			return result, errors.New("invalid GeoJSON: malformed Polygon")
+		}
+		result.Polygons = append(result.Polygons, poly)
+
+	case "MultiPoint":
+		var mp MultiPoint
+		err = json.Unmarshal(data, &mp)
+		if err != nil {
+			return result, errors.New("invalid GeoJSON: malformed MultiPoint")
+		}
+		result.MultiPoints = append(result.MultiPoints, mp)
+
+	case "MultiLineString":
+		var mls MultiLineString
+		err = json.Unmarshal(data, &mls)
+		if err != nil {
+			return result, errors.New("invalid GeoJSON: malformed MultiLineString")
+		}
+		result.MultiLineStrings = append(result.MultiLineStrings, mls)
+
+	case "MultiPolygon":
+		var mpoly MultiPolygon
+		err = json.Unmarshal(data, &mpoly)
+		if err != nil {
+			return result, errors.New("invalid GeoJSON: malformed MultiPolygon")
+		}
+		result.MultiPolygons = append(result.MultiPolygons, mpoly)
+
+	case "GeometryCollection":
+
+		var partial partialGeometryCollection
+		err = json.Unmarshal(data, &partial)
+		if err != nil {
+			return result, errors.New("invalid GeoJSON: malformed GeometryCollection")
+		}
+
+		var subresult GeoJSONContents
+		for i := 0; i != len(partial.Geometries); i++ {
+			subresult, err = UnmarshalGeoJSON(partial.Geometries[i])
+			if err != nil {
+				return result, err
+			}
+
+			result.Points = append(result.Points, subresult.Points...)
+			result.LineStrings = append(result.LineStrings, subresult.LineStrings...)
+			result.Polygons = append(result.Polygons, subresult.Polygons...)
+			result.MultiPoints = append(result.MultiPoints, subresult.MultiPoints...)
+			result.MultiLineStrings = append(result.MultiLineStrings, subresult.MultiLineStrings...)
+			result.MultiPolygons = append(result.MultiPolygons, subresult.MultiPolygons...)
+		}
+
 	case "Feature":
 		var feature Feature
 		err = json.Unmarshal(data, &feature)
@@ -44,93 +120,16 @@ func UnmarshalGeoJSON(data []byte) (GeoJSONResult, error) {
 			return result, errors.New("invalid GeoJSON: malformed Feature")
 		}
 		result.Features = append(result.Features, feature)
+
 	case "FeatureCollection":
 		var featureCollection FeatureCollection
 		err = json.Unmarshal(data, &featureCollection)
 		if err != nil {
 			return result, errors.New("invalid GeoJSON: malformed FeatureCollection")
 		}
-		for i := 0; i != len(featureCollection.Features); i++ {
-			result.Features = append(result.Features, featureCollection.Features[i])
-		}
+		result.Features = append(result.Features, featureCollection.Features...)
 	default:
-		g, err := UnmarshalGeometry(data)
-		if err != nil {
-			return result, err
-		}
-		result.Geometries = append(result.Geometries, g)
+		return result, errors.New(fmt.Sprintf("unrecognized type: %s", uknType.Type))
 	}
 	return result, nil
-}
-
-// UnmarshalGeometry recursively unpacks Geometries (including GeometryCollections) from a JSON byte array
-func UnmarshalGeometry(data []byte) (Geometry, error) {
-	var uknType unknownGeoJSONType
-	err := json.Unmarshal(data, &uknType)
-	if err != nil {
-		return new(Point), err
-	}
-	switch uknType.Type {
-	case "Point":
-		var pt Point
-		err = json.Unmarshal(data, &pt)
-		if err != nil {
-			return pt, errors.New("invalid GeoJSON: malformed Point")
-		}
-		return pt, nil
-	case "LineString":
-		var ls LineString
-		err = json.Unmarshal(data, &ls)
-		if err != nil {
-			return ls, errors.New("invalid GeoJSON: malformed LineString")
-		}
-		return ls, nil
-	case "Polygon":
-		var poly Polygon
-		err = json.Unmarshal(data, &poly)
-		if err != nil {
-			return poly, errors.New("invalid GeoJSON: malformed Polygon")
-		}
-		return poly, nil
-	case "MultiPoint":
-		var mp MultiPoint
-		err = json.Unmarshal(data, &mp)
-		if err != nil {
-			return mp, errors.New("invalid GeoJSON: malformed MultiPoint")
-		}
-		return mp, nil
-	case "MultiLineString":
-		var mls MultiLineString
-		err = json.Unmarshal(data, &mls)
-		if err != nil {
-			return mls, errors.New("invalid GeoJSON: malformed MultiLineString")
-		}
-		return mls, nil
-	case "MultiPolygon":
-		var mpoly MultiPolygon
-		err = json.Unmarshal(data, &mpoly)
-		if err != nil {
-			return mpoly, errors.New("invalid GeoJSON: malformed MultiPolygon")
-		}
-		return mpoly, nil
-	case "GeometryCollection":
-		var collection GeometryCollection
-		var partial partialGeometryCollection
-		err = json.Unmarshal(data, &partial)
-		if err != nil {
-			fmt.Println(err)
-			return collection, errors.New("invalid GeoJSON: malformed GeometryCollection")
-		}
-		var geom Geometry
-		for i := 0; i != len(partial.Geometries); i++ {
-			geom, err = UnmarshalGeometry(partial.Geometries[i])
-			if err != nil {
-				return collection, err
-			}
-			collection.Geometries = append(collection.Geometries, geom)
-		}
-		return collection, nil
-	default:
-		return new(Point), errors.New(fmt.Sprintf("unrecognized GeoJSON type '%s'", uknType.Type))
-	}
 }
